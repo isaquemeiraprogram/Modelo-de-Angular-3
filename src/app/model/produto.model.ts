4bf13036253ef674594b4addcf8d331c7b1541fa
isak Adm <isaque.admeira@gmail.com>
2025-06-09 20:27:23 -0300

    first commit

---
+++ src/app/model/produto.model.ts
@@ -0,0 +1,8 @@
+// export = pra deixar acessivel = public
+// interface= o que faz o codigo entender como uma especie de construtor da classe
+export interface Produto {
+    img: string
+    nome: string
+    preco: number
+}
+
